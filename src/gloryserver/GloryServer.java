9c5bc44e047e00cca4c12417a6407eccfc734813
tnlthanzeel <tnlthanzeel@gmail.com>
2018-06-24 16:15:25 +0530

    server fixes

--- src/gloryserver/GloryServer.java
+++ src/gloryserver/GloryServer.java
@@ -5,8 +5,9 @@
  */
 package gloryserver;
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -20,40 +21,83 @@ public class GloryServer {
     static String Username;
     static int NumberOfPlayers = 0;
     static String[] connectedUsers = new String[4];
+    static boolean firstPlayer = false;
 
     public static void main(String[] args) {
+
         try {
             int port = 25000;
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server Started and listening to the port 25000");
+
             while (true) {
+
                 socket = serverSocket.accept();
+                Executors.newCachedThreadPool().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            InputStream is1 = socket.getInputStream();
+                            InputStreamReader isr1 = new InputStreamReader(is1);
+                            BufferedReader br1 = new BufferedReader(isr1);
+                            String username1 = br1.readLine();
+                            System.out.println("Message received from client is " + username1);
 
-                if (NumberOfPlayers < 4) {
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String username = br.readLine();
-                    connectedUsers[NumberOfPlayers] = username;
-                    System.out.println("Message received from client is " + username);
-                    NumberOfPlayers++;
-                }
+                            if (firstPlayer && NumberOfPlayers == 1 && connectedUsers[0].equals(username1)) {
+                                boolean isTwo = false;
+                                while (!isTwo) {
+                                    System.out.println("Message received from client is " + username1);
+                                    OutputStream os = socket.getOutputStream();
+                                    OutputStreamWriter osw = new OutputStreamWriter(os);
+                                    BufferedWriter bw = new BufferedWriter(osw);
+                                    bw.write(NumberOfPlayers + "\n");
+                                    System.out.println("Message sent to the client is " + NumberOfPlayers + "\n");
+                                    bw.flush();
+                                    if (NumberOfPlayers >= 2) {
+                                        isTwo = true;
+                                    }
+                                }
+                                //continue ;
+                            }
 
-                if (NumberOfPlayers >= 2 && NumberOfPlayers <= 4) {
-                    System.out.println("Staring for " + NumberOfPlayers + " players");
-//                    OutputStream os = socket.getOutputStream();
-//                    OutputStreamWriter osw = new OutputStreamWriter(os);
-//                    BufferedWriter bw = new BufferedWriter(osw);
-//                    bw.write(NumberOfPlayers+"\n");
-//                    System.out.println("Message sent to the client is " + NumberOfPlayers + "\n");
-//                    bw.flush();
-                }
-                OutputStream os = socket.getOutputStream();
-                    OutputStreamWriter osw = new OutputStreamWriter(os);
-                    BufferedWriter bw = new BufferedWriter(osw);
-                    bw.write(NumberOfPlayers+"\n");
-                    System.out.println("Message sent to the client is " + NumberOfPlayers + "\n");
-                    bw.flush();
+                            if (NumberOfPlayers < 4) {
+
+                                if (NumberOfPlayers == 0) {
+                                    connectedUsers[NumberOfPlayers] = username1;
+                                    NumberOfPlayers++;
+                                    System.out.println("Message received from client is " + username1);
+                                } else if (connectedUsers[NumberOfPlayers] != (username1)) {
+                                    connectedUsers[NumberOfPlayers] = username1;
+                                    NumberOfPlayers++;
+                                    System.out.println("Message received from client is " + username1);
+                                }
+
+                                if (NumberOfPlayers == 1) {
+                                    firstPlayer = true;
+                                } else {
+                                    firstPlayer = false;
+                                }
+                            }
+
+                            if (NumberOfPlayers >= 2 && NumberOfPlayers <= 4) {
+                                System.out.println("Staring for " + NumberOfPlayers + " players");
+
+                            }
+                            // if (NumberOfPlayers != 1) {
+                            OutputStream os = socket.getOutputStream();
+                            OutputStreamWriter osw = new OutputStreamWriter(os);
+                            BufferedWriter bw = new BufferedWriter(osw);
+                            bw.write(NumberOfPlayers + "\n");
+                            System.out.println("Message sent to the client is " + NumberOfPlayers + "\n");
+                            bw.flush();
+//                            }
+
+// code goes here.
+                        } catch (Exception ex) {
+                        }
+                    }
+                    ///}).start();
+                });
             }
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
